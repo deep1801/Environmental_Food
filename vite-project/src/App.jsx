0a8ee1ac35f3879493f98b3cc0e6f18ac0113eb0
Pradeep Yadav <pradeepyadav20036@gmail.com>
2024-04-04 16:35:27 +0530

    pages added

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import WeDo from './WeDo'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <WeDo/>
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/WeDo.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function WeDo(){
+    return(
+        <div className="font-normal flex flex-col   text-[400] text-start gap-3">
+            <p className="text-[3.5rem] w-[40rem] xl:w-[60rem] border border-red-500 font-[500] lg:text-[3rem] text-[#BA007A]">New Food Product Development</p>
+            <div className="flex flex-col gap-3">
+                <p className="font-color text-[1.5rem] " >Turning your Vision into a Reality</p>
+                <p className="text-color font-[600] text-[#BA007A] w-[30rem] text-[1.3rem] border-black">Need help with developing a new food product to introduce in the market or customise according to consumer preferences? Or a Creative product that can be used to promote your brand?</p>
+                <p className="hero  w-[65rem] xl:w-[70rem]border -red-500 font-[500]">The contract Research and Development (R&D) team of Guires Food Research Lab (FRL) has vast experience designing and developing new food products tailored to the client’s goals. By working directly with Guires expertise, you’re assured of getting a dynamic, innovative food product of your specifications. We keep a close eye on food industry trends and assist manufacturing companies with new product formulation creation and re-formulation to match consumers’ ever-changing desire for healthy and flavourful food. We also help with a cost analysis of new products for your specification and certification process of your product development.</p>
+            </div>
+            <div className="prop text-[2rem] font-[200] flex-col gap-11">
+                <p className="text-[1.5rem] font-semibold">What are the different types of new food product development (FPD) that you offer?</p>
+            </div>
+           <div className="flex flex-col gap-4">
+           <p><span className="text-black w-20 h-10 font-extrabold">.</span>New to World: We help you with the innovative product (new to the world) which is entirely unique and different to pre-existing products. Example new technologies include genetically modified foods, extrusion, 3D printing, new ingredients etc.</p>
+            <p className="flex flex-col gap -3 w-70">Line Extension: addition to existing product lines: We help you with line extension of your current SKU to meet consumer needs and increase market share or to reposition your products in a way to attract different kinds of consumers or significant improvement. The processing and formulation are similar to a current product. We change the product considering the diet-related factors (e.g., low fat, high fibre, salt-reduced, special dietary requirements that is gluten-free or dairy-free, packing size, or features, health, colour and flavour. We can also work on cost reduction of your existing product and repositioning.</p>
+            <p className="flex flex-col gap-3 ">Me too Products: We help to copy existing products as processing methods are well known and ingredients already available.</p>
+            <p className="flex flex-col gap-3">Me too Products: We help to copy existing products as processing methods are well known and ingredients already available.</p>
+           </div>
+        </div>
+        
+    )
+}
+export default WeDo
